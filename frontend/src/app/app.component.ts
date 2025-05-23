2912972d091a2699343e055876a21706ad9b8b34
Rui Cavalcanti <sruicavalcanti@gmail.com>
2025-05-23 11:54:29 -0300

    Criando o footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component';
+import {FooterComponent} from './components/template/footer/footer.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent, FooterComponent],
   templateUrl: 'app.component.html' 
 })
 export class AppComponent {  
