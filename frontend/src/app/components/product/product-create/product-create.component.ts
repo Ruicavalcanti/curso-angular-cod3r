3897d962feb9b70aab2630d3a9b4abbcbbff732e
Rui Cavalcanti <sruicavalcanti@gmail.com>
2025-06-05 15:32:20 -0300

    Consumindo api para cadastrar um produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from './../product.service';
 import { MatButtonModule } from '@angular/material/button';
+import { Product } from '../product.model';
 @Component({
   selector: 'app-product-create',
   imports: [MatButtonModule],
@@ -9,13 +10,20 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './product-create.component.css'
 })
 export class ProductCreateComponent {
-constructor(private productService: ProductService,
+  product: Product = {
+    name: 'Produto de Teste',
+    price: 125.98
+  }
+  constructor(private productService: ProductService,
     private router: Router) { }
 
-
-
   createProduct(): void {
-    this.productService.showMessage('Produto criado!');
+    this.productService.create(this.product)
+      .subscribe(() => {
+        this.productService.showMessage('Produto criado!');
+        this.router.navigate(['/products'])
+      })
+
   }
   cancel(): void {
     this.router.navigate(['/products'])
