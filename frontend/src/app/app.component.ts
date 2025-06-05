3e21bf3a81af059da18eace87710d3cd7bae0f2e
Rui Cavalcanti <sruicavalcanti@gmail.com>
2025-06-05 10:31:07 -0300

    Criar Componente Produto

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component';
 import {FooterComponent} from './components/template/footer/footer.component'
 import { NavComponent } from './components/template/nav/nav.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent, FooterComponent,NavComponent],
+  imports: [RouterOutlet,HeaderComponent, FooterComponent,NavComponent,ProductCreateComponent],
   templateUrl: 'app.component.html' 
 })
 export class AppComponent {  
