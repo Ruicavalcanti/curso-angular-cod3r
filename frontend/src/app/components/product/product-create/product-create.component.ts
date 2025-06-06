b7533a55807d8d6c0080c0eba6c25d554b3f95a9
Rui Cavalcanti <sruicavalcanti@gmail.com>
2025-06-05 22:11:12 -0300

    Criando Formulario Criar Produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -3,16 +3,22 @@ import { Router } from '@angular/router';
 import { ProductService } from './../product.service';
 import { MatButtonModule } from '@angular/material/button';
 import { Product } from '../product.model';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card'; // ✅ adicione isso
+
+
 @Component({
   selector: 'app-product-create',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,FormsModule,MatFormFieldModule,MatInputModule,MatCardModule],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css'
 })
 export class ProductCreateComponent {
   product: Product = {
-    name: 'Produto de Teste',
-    price: 125.98
+    name: '',
+    price: 0.00
   }
   constructor(private productService: ProductService,
     private router: Router) { }
